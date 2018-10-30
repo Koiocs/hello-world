3e9de2f1ef8bfa766cdf69c9fe34916e2beacc7c
Koiocs <549154383@qq.com>
2018-10-30 17:20:15 +0800

    Cao

---
+++ codes/XJOI/5-1/word.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int d1,d2,w1,w2;
+inline int read() {
+   int s=0,w=1;
+   char ch=getchar();
+   while(ch<'0'||ch>'9'){if(ch=='-')w=-1;ch=getchar();}
+   while(ch>='0'&&ch<='9') s=s*10+ch-'0',ch=getchar();
+   return s*w;
+}
+int main() {
+    d1=read(),d2=read(),w1=read(),w2=read();
+
+    return 0;
+}
+//2333
